495bb99d467a1be99efd980ef417baa23deb41a8
Artem Litvintsev <61841951+fatarick@users.noreply.github.com>
2024-09-19 15:35:04 +0300

    Add files via upload

---
+++ countdown.py
@@ -0,0 +1,26 @@
+import sys
+from PyQt6.QtWidgets import QWidget, QApplication, QVBoxLayout
+from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt6.QtCore import QUrl, Qt
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Countdown App")
+
+        browser = QWebEngineView()
+        layout = QVBoxLayout()
+        layout.addWidget(browser)
+
+        browser.setUrl(QUrl("")) # Set the URL here
+        self.resize(450, 325)
+        self.setLayout(layout)
+        self.setWindowFlag(Qt.WindowType.WindowStaysOnTopHint, True)
+        self.show()
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+app.exec()
